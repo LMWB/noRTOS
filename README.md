3f9f78144014c5f056472a356eb37de195aeddfe
MaxWilli86 <lukas.bartels@smartcube360.de>
2024-12-01 20:42:27 +0100

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,39 @@ rudimentary C-library for microcontrollers to organisation and run various task 
 
 # Prefact
 I'm sure there are many thousand different very good approaches out there on how to organize and manage multiple things do on a single cpu low budget microcontroller **without a RTOS**, but still I would like to share mine.
+
+# How To Use
+Copy paste the ```noRTOS.c, noRTOS.h, utils.c, utils.h``` files to your project tree.
+Copy paste ```hardwareGlobal.h``` to you project tree and modify line ```19 #define GET_TICK()``` according to your platform.
+
+In ```main.c``` right before the ```int main(void)```  do the following. Create some callback function you like to run. Consider each as individual **non blocking** main()-loops.   
+
+```
+void blinky(void) {
+    LED_toggle();
+}
+
+void test_callback1(void) {
+    printf("Testing printf with _write() override\n\n");
+}
+
+```
+Than right before ```while(1)``` super-loop create instances of ```noRTOS_task_t``` and add them to the scheduler.   
+
+```
+noRTOS_task_t blinky_t = { .delay = eNORTOS_PERIODE_1s, .task_callback = blinky };
+noRTOS_add_task_to_scheduler(&blinky_t);
+
+noRTOS_task_t test_task1 = { .delay = eNORTOS_PERIODE_5s, .task_callback = test_callback1 };
+noRTOS_add_task_to_scheduler(&test_task1);
+
+```
+Finally start the scheduler   
+
+```
+/* this runs for ever */
+noRTOS_run_scheduler();
+/* never get here! */
+while (1) {}
+
+```
--- Software/Nucleo-F446RE-simple-example/Core/Src/main.c
+++ Software/Nucleo-F446RE-simple-example/Core/Src/main.c
@@ -64,41 +64,9 @@ void SystemClock_Config(void);
 /* USER CODE BEGIN 0 */
 static uint8_t global_button_state = 0;
 static bool global_button_event = false;
-/* USER CODE END 0 */
 
-/**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
-
-	/* USER CODE BEGIN 1 */
-
-	/* USER CODE END 1 */
-
-	/* MCU Configuration--------------------------------------------------------*/
-
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
-
-	/* USER CODE BEGIN Init */
-
-	/* USER CODE END Init */
-
-	/* Configure the system clock */
-	SystemClock_Config();
-
-	/* USER CODE BEGIN SysInit */
-
-	/* USER CODE END SysInit */
-
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_USART2_UART_Init();
-	/* USER CODE BEGIN 2 */
-
-	/* create some callback function you like to run
-	 * consider each as individual non blocking main()-loops*/
+/* create some callback function you like to run
+ * consider each as individual non blocking main()-loops*/
 	void read_button_state(void){
 		uint8_t button = NUCLEO_BUTTON_READ();
 			if (button != global_button_state) {
@@ -142,17 +110,51 @@ int main(void) {
 		printf("\t\tHello World Task 3\n");
 	}
 
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+
+	/* USER CODE BEGIN 1 */
+
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	/* USER CODE BEGIN 2 */
+
+
 	/* now I create some tasks and add them to the scheduler */
-	noRTOS_task_t button = { .delay = eNORTOS_PERIODE_10milli, .task_callback = read_button_state };
+	noRTOS_task_t button = { .delay = eNORTOS_PERIODE_10ms, .task_callback = read_button_state };
 	noRTOS_add_task_to_scheduler(&button);
 
-	noRTOS_task_t task_communication = { .delay = eNORTOS_PERIODE_100milli, .task_callback = inter_task_communication };
+	noRTOS_task_t task_communication = { .delay = eNORTOS_PERIODE_100ms, .task_callback = inter_task_communication };
 	noRTOS_add_task_to_scheduler(&task_communication);
 
 	noRTOS_task_t heartbeat = { .delay = eNORTOS_PERIODE_1s, .task_callback = blinky };
 	noRTOS_add_task_to_scheduler(&heartbeat);
 
-	noRTOS_task_t task_timing = { .delay = eNORTOS_PERIODE_500milli, .task_callback = timing };
+	noRTOS_task_t task_timing = { .delay = eNORTOS_PERIODE_500ms, .task_callback = timing };
 	noRTOS_add_task_to_scheduler(&task_timing);
 
 	noRTOS_task_t test_task1 = { .delay = eNORTOS_PERIODE_1s, .task_callback = test_callback1 };
