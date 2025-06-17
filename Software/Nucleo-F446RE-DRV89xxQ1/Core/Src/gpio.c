b79466af12639a1d64f0a302fbb3ea18808de8e5
MaxWilli86 <lukas.bartels@smartcube360.de>
2025-06-17 10:58:39 +0200

    IC acting strange, no output yet

--- Software/Nucleo-F446RE-DRV89xxQ1/Core/Src/gpio.c
+++ Software/Nucleo-F446RE-DRV89xxQ1/Core/Src/gpio.c
@@ -62,19 +62,12 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : LD2_Pin SPI1_nCS_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin|SPI1_nCS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : SPI1_nCS_Pin */
-  GPIO_InitStruct.Pin = SPI1_nCS_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(SPI1_nCS_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
--- Software/Nucleo-F446RE-DRV89xxQ1/Core/Src/main.c
+++ Software/Nucleo-F446RE-DRV89xxQ1/Core/Src/main.c
@@ -13,6 +13,14 @@
  * in the root directory of this software component.
  * If no LICENSE file comes with this software, it is provided AS-IS.
  *
+ *
+ * Testing DRV8908 Digital Out Driver from TI
+ * Setup:
+ * Nucleo-F446RE
+ * SPI on PA10, PB4, PB5, PB6
+ *
+ *
+ *
  ******************************************************************************
  */
 /* USER CODE END Header */
@@ -26,8 +34,10 @@
 /* USER CODE BEGIN Includes */
 
 /* Control Register */
-#define OP_CTRL_1 0x08
-#define OP_CTRL_2 0x09
+#define IC_STAT		0x01
+#define CONFIG_CTRL 0x07
+#define OP_CTRL_1 	0x08
+#define OP_CTRL_2 	0x09
 
 /* Bitmask */
 #define HB1_LS_EN (1<<0)
@@ -48,6 +58,10 @@
 #define HB8_LS_EN (1<<6)
 #define HB8_HS_EN (1<<7)
 
+// Helper macros for CS control
+#define DRV_CS_LOW()   HAL_GPIO_WritePin(SPI1_nCS_GPIO_Port, SPI1_nCS_Pin, 0)
+#define DRV_CS_HIGH()  HAL_GPIO_WritePin(SPI1_nCS_GPIO_Port, SPI1_nCS_Pin, 1)
+
 void drv89xxq1_enable_chip_select(void) {
 	HAL_GPIO_WritePin(SPI1_nCS_GPIO_Port, SPI1_nCS_Pin, 0);
 }
@@ -64,20 +78,20 @@ void drv89xxq1_write_control_register(uint8_t register_address, uint8_t data, ui
 	HAL_SPI_TransmitReceive(&hspi1, spi_tx_data_frame, result, 2, 0xffff);
 }
 
-void drv89xxq1_read_control_register(uint8_t register_address, uint8_t data, uint8_t *result) {
+void drv89xxq1_read_control_register(uint8_t register_address, uint8_t *result) {
 	/* compose the write address byte */
 	uint16_t address = 0x40;// For a read command (W0 = 1), the response word is the data currently in the register being read.
 	address |= register_address;
-	uint8_t spi_tx_data_frame[2] = { address, data };
+	uint8_t spi_tx_data_frame[2] = { address, 0xff};
 	HAL_SPI_TransmitReceive(&hspi1, spi_tx_data_frame, result, 2, 0xffff);
 }
 
-void drv89xxq1_set_outputs(uint8_t bank, uint8_t output_state_bitmask) {
-	uint8_t result[2] = { 0 };
+void drv89xxq1_set_outputs(uint8_t bank, uint8_t output_state_bitmask, uint8_t *result) {
 	drv89xxq1_enable_chip_select();
 	drv89xxq1_write_control_register(bank, output_state_bitmask, result);
 	drv89xxq1_disable_chip_select();
 }
+
 
 /* USER CODE END Includes */
 
@@ -114,102 +128,120 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
 
-	/* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_USART2_UART_Init();
-	MX_SPI1_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_SPI1_Init();
+  /* USER CODE BEGIN 2 */
 
-	/* USER CODE END 2 */
+  uint8_t result[2] = {0};
+  drv89xxq1_enable_chip_select();
+  drv89xxq1_read_control_register(CONFIG_CTRL, result);
+  drv89xxq1_disable_chip_select();
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  drv89xxq1_enable_chip_select();
+  drv89xxq1_write_control_register(CONFIG_CTRL, 0, result);
+  drv89xxq1_disable_chip_select();
+
+  HAL_Delay(500);
+  drv89xxq1_enable_chip_select();
+  drv89xxq1_read_control_register(IC_STAT, result);
+  drv89xxq1_enable_chip_select();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1) {
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 		for (uint16_t i = 0; i < 8; ++i) {
 			uint8_t output = (1 << i);
-			drv89xxq1_set_outputs(OP_CTRL_1, output);
+			drv89xxq1_set_outputs(OP_CTRL_1, output, result);
 			HAL_Delay(500);
 		}
 
-		for (uint16_t i = 0; i < 8; ++i) {
+		for (uint16_t i = 1; i < 8; ++i) {
 			uint8_t output = (1 << i);
-			drv89xxq1_set_outputs(OP_CTRL_2, output);
+			drv89xxq1_set_outputs(OP_CTRL_2, output, result);
 			HAL_Delay(500);
 		}
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	RCC_OscInitStruct.PLL.PLLM = 8;
-	RCC_OscInitStruct.PLL.PLLN = 96;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 2;
-	RCC_OscInitStruct.PLL.PLLR = 2;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -217,16 +249,17 @@ void SystemClock_Config(void) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
